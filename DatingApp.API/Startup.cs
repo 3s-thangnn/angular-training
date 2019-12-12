08e04b1353a4a5ad8214202e2443bd8596af694e
Thang Nguyen <thang.nn@3si.vn>
2019-12-12 15:49:35 +0700

    Get data by call api

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => 
                 x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +38,10 @@ namespace DatingApp.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
